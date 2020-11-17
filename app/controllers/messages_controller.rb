f4de7c11cb5c89e996608a2e8aa45d0fdc17186f
chulhee23 <chulhee2351@gmail.com>
2020-11-17 16:08:35 +0900

    Remove welcome controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
 
-  # GET /messages
   # GET /messages.json
   def index
     respond_to do |format|
@@ -10,18 +9,9 @@ class MessagesController < ApplicationController
     end
   end
 
-  # GET /messages/1
-  # GET /messages/1.json
-  def show
-  end
-
   # GET /messages/new
   def new
     @messages = Message.all.order(created_at: :asc)
-  end
-
-  # GET /messages/1/edit
-  def edit
   end
 
   # POST /messages
@@ -36,30 +26,6 @@ class MessagesController < ApplicationController
     }) if @message.present?
     head :ok
 
-  end
-
-  # PATCH/PUT /messages/1
-  # PATCH/PUT /messages/1.json
-  def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to @message, notice: 'Message was successfully updated.' }
-        format.json { render :show, status: :ok, location: @message }
-      else
-        format.html { render :edit }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /messages/1
-  # DELETE /messages/1.json
-  def destroy
-    @message.destroy
-    respond_to do |format|
-      format.html { redirect_to messages_url, notice: 'Message was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
